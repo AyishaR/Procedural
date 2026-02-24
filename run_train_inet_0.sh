129198537eccee482acb9b354c2661a41573e792
Ayisha Ryhana Dawood <dawooda@informatik.uni-freiburg.de>
2026-02-24 11:51:00 +0100

    add .sh for imagenet

---
+++ run_train_inet_0.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#SBATCH --job-name inet_random
+#SBATCH --partition lmbhiwidlc_gpu-rtx2080
+#SBATCH --nodes 1
+#SBATCH --gres=gpu:4
+#SBATCH --time 23:59:59
+#SBATCH -o /home/dawooda/code/procedural/Procedural/logs/inet_%j_%x.out
+#SBATCH -e /home/dawooda/code/procedural/Procedural/logs/inet_%j_%x.err # STDERR
+#SBATCH --mail-type END,FAIL 
+
+ROOT='/home/dawooda/code/procedural'
+
+cd $ROOT
+echo "Started at $(date)";
+
+echo "Running job $SLURM_JOB_NAME using $SLURM_GPUS_ON_NODE gpus per node with given JID $SLURM_JOB_ID on queue $SLURM_JOB_PARTITION";
+
+source ~/.bashrc
+conda activate prvenv
+
+cd Procedural
+echo "Current working directory: $(pwd)";
+
+nvidia-smi
+echo "CUDA_VISIBLE_DEVICES = $CUDA_VISIBLE_DEVICES"
+
+TOTAL_BATCH_SIZE=4096
+BATCH_SIZE=64
+UPDATE_FREQ=$((($TOTAL_BATCH_SIZE / $SLURM_GPUS_ON_NODE) / $BATCH_SIZE))
+
+for i in 0; do
+    torchrun --standalone --nproc_per_node=$SLURM_GPUS_ON_NODE main.py \
+        --model vit_base  --warmup_epochs 50 --epochs 300 \
+        --total_batch_size $TOTAL_BATCH_SIZE \
+        --batch_size $BATCH_SIZE --lr 2e-3 --update_freq $UPDATE_FREQ --use_amp true \
+        --data_path "/data/datasets/ILSVRC2012" \
+        --data_set "IMNET" \
+        --output_dir "/home/dawooda/code/procedural/Procedural/results_random_inet/s$i" \
+        --enable_wandb true \
+        --project "imagenet" \
+        --slurm_id 10000001 \
+        --seed $i
+
+    sleep 10
+done
+
+echo "DONE";
+echo "Finished at $(date)";
